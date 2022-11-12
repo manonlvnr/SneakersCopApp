06008a7e166578c07a84c272ac5bc386532b9fb5
Manon Lavenier <manon.lavenier@gmail.com>
2022-11-12 17:46:04 +0100

    Create the bottom tab navigation

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import Home from './component/home';
+import Calendar from './component/calendar';
+import Account from './component/account';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Calendar') {
+              iconName = focused ? 'calendar' : 'calendar-outline';
+            } else if (route.name === 'Account') {
+              iconName = focused ? 'people' : 'people-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#808080',
+          tabBarInactiveTintColor: '#C0C0C0',
+        })}
+        >        
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Calendar" component={Calendar} />
+        <Tab.Screen name="Account" component={Account} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
