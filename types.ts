5811570e735a9f8c5b600ad69179c55b9f738263
Manon Lavenier <manon.lavenier@gmail.com>
2022-11-12 22:25:33 +0100

    Create the data needed

---
+++ types.ts
@@ -0,0 +1,10 @@
+export interface RandomSneakersJSON {
+    name: string,
+    price: number,
+    image: string,
+    releaseDate: string,
+    colorName: string,
+    colorHex: string,
+    resell: string,
+    resellShop: string[],
+  }
